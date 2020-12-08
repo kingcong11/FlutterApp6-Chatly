12fe2af4f54984c63f169a32e710c9c478e1b74f
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2020-12-09 06:24:23 +0800

    initial environment setup
    *added commonly used packages
    *added assets essentials

---
+++ lib/screens/homepage_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatelessWidget {
+  /* Properties */
+  static const routeName = '/homepage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('HOMEPAGE'),
+      ),
+    );
+  }
+}
